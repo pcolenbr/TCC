538a63f814a2c5bc892bed40558a116ad0644da7
Pedro Colen Cardoso <pedro@dreamt.com>
2015-08-17 22:00:41 -0300

    No message

--- Pratica/src/controller.js
+++ Pratica/src/controller.js
@@ -10,11 +10,12 @@ app.controller('appController', function ($scope, $http) {
 	var apiKey = "A.b5b826959702a0a5b767f9d822bc15a1";
 
 	$scope.executeTest = function () {
-		$http.post("http://www.webpagetest.org/runtest.php?url=www.aol.com&k=" + apiKey).then(function (results) {
+		$http.post("http://www.webpagetest.org/runtest.php?url=www.aol.com&f=json&k=" + apiKey).then(function (results) {
 			console.log(results);
-			var myWindow = window.open("data:text/html," + encodeURIComponent(results.data),
-			                       "_blank");
-			myWindow.focus();
+			
+			// var myWindow = window.open("data:text/html," + encodeURIComponent(results.data),
+			//                        "_blank");
+			// myWindow.focus();
 		});
 	};
 });
