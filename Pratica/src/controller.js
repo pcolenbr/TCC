3141d0ec95ebee22b67e6b14c761c4a5f0587aed
Pedro Colen Cardoso <pedro@dreamt.com>
2015-08-19 20:59:38 -0300

    testing script

--- Pratica/src/controller.js
+++ Pratica/src/controller.js
@@ -9,43 +9,49 @@ app.controller('appController', function ($scope, $http, $interval) {
 	$scope.testResult = {};
 
 	var apiKey = "A.b5b826959702a0a5b767f9d822bc15a1";
-	var testFinish;
+	var testFinish = {};
 	var browsersLocations = {
 		chrome: 'ec2-us-east-1:Chrome',
-		ie: 'ec2-us-east-1:IE 11'
-		// ,
-		// firefox: 'ec2-us-east-1:Firefox',
-		// safari: 'ec2-us-east-1:Safari'
+		ie: 'ec2-us-east-1:IE 11',
+		firefox: 'ec2-us-east-1:Firefox',
+		safari: 'ec2-us-east-1:Safari'
 	}
 
 	var testPoll = function(url, key){
-		testFinish = $interval(function () {			            
+		testFinish[key] = $interval(function () {			            
         	$http.get(url).then(function (results) {
+        		console.log("2 - " + key);
 				if(results.data.statusCode == 200) {
-					$scope.testResult.key = results.data;
-					$scope.stopTest();
+					console.log(200);
+					$scope.testResult[key] = results.data;
+					stopTest(key);
 				}			
 			});    
         }, 5000);
-	}
+        console.log(testFinish);
+	};
 
-	$scope.stopTest = function () {
-		$interval.cancel(testFinish);
-		console.log($scope.testResult);
+	var stopTest = function (key) {
+		$interval.cancel(testFinish[key]);
+		console.log("kill");
+		console.log(testFinish[key]);
+	};
+
+	var startTest = function (url, key) {
+		$scope.testResult[key] = {};
+		$http.post(url).then(function (results) {
+			console.log(results.data.data.jsonUrl);
+			testPoll(results.data.data.jsonUrl, key);
+		});
 	}
 
 	$scope.executeTest = function () {
 		var testUrl = '';
 		for (var key in browsersLocations) {
 			testUrl = "http://www.webpagetest.org/runtest.php?url=" + $scope.testData.url + "&k=" + apiKey + "&runs=" + $scope.testData.times + "&location=" + browsersLocations[key] + "&label=" + key + "&f=json&noimages=1";
-
-			$http.post(testUrl).then(function (results) {
-				console.log(results.data.data.jsonUrl);
 			
-				testPoll(results.data.data.jsonUrl, key);
-			});
+			startTest(testUrl, key);
 		}
 	};
-
 
 });
